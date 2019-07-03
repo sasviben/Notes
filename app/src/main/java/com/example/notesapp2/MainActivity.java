cf5faf67f9ba91328ee25d7512f324643ab69462
SandroSviben <sandro.sviben@agrivi.com>
2019-07-03 14:52:38 +0200

    Implementirana Delete all funkcionalnost

--- app/src/main/java/com/example/notesapp2/MainActivity.java
+++ app/src/main/java/com/example/notesapp2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.notesapp2;
 
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -18,6 +20,7 @@ import android.view.MenuItem;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import com.example.notesapp.DBOpenHelper;
 
@@ -74,7 +77,30 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
+        if (id == R.id.action_delete_all) {
+            deleteAllNotes();
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteAllNotes() {
+        DialogInterface.OnClickListener diaClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int button) {
+                if (button == DialogInterface.BUTTON_POSITIVE) {
+                    getContentResolver().delete(NotesProvider.CONTENT_URI, null, null);
+
+                    Toast.makeText(MainActivity.this, getString(R.string.all_deleted), Toast.LENGTH_SHORT);
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.are_you_sure)
+                .setPositiveButton(getString(android.R.string.yes), diaClickListener)
+                .setNegativeButton(getString(android.R.string.no), diaClickListener)
+                .show();
+
     }
 }
